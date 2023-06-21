5162ebee300a7143b0b3cd89054ecda880a5b909
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-21 11:01:10 +0530

    updated header&footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () =>{
     return (
         <div className="bg-orange-300 h-16 text-center pt-4 sticky top-full">
-            <h2 className="font-sans font-bold">This is Footer</h2>
+            <h2 className="font-sans font-bold">Made with ❤ by <a href="https://github.com/Ashutosh-Patel028/" target="__blank">Ashutosh</a></h2>
         </div>
     );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 
 const Header = ()=>{
     return (
-        <div className="h-20 border border-4 border-red-200 text-center pt-4 bg-orange-300">
+        <div className="h-20 border-red-200 text-center pt-4 bg-orange-300">
             <h1 className="font-serif font-bold ">Programming-Essential-Calculator</h1>
         </div>
     );
